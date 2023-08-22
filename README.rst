21ea65a93c35874748920a8bd61d6db7b64b2d75
Nils Wireklint <nils@meroton.com>
2023-08-22 10:21:55 +0200

    Add c prototypes

--- README.rst
+++ README.rst
@@ -6,3 +6,69 @@ Implement "mountat" with the new mount API.
 README is coming soon,
 I'm writing a blog post about the background
 and will tie the docs together after that.
+
+Goals
+=====
+
+✅Mountat
+❌Umountat
+
+Go programs
+===========
+
+.. TODO
+
+C prototypes
+============
+
+The `c-prototypes/` directory contains two files
+that have the simplest implementation of using the syscalls.
+Before writing good error-checking go code I wrote these to prototype.
+To understand errors I recommend using `strace`
+to see how the syscalls are called and what they return.
+
+The first file `mountat.c` shows how to implement the barest `mountat`.
+This mounts `/proc` and `/sys` into the current working directory,
+no arguments are handled.
+
+::
+
+    $ gcc mountat.c
+    $ mktemp -d
+    /tmp/tmp.MlpdhEhm8g
+    $ prog=$PWD/a.out
+    $ cd /tmp/tmp.MlpdhEhm8g
+
+
+    $ mkdir sys
+    $ mkdir proc
+    $ tree
+    .
+    ├── proc
+    └── sys
+    $ sudo "$prog"
+    $ tree -L 2 | tail -1
+    736 directories, 47 files
+
+The second file does the same but with a directory file descriptor,
+a small improvement in functionality.
+
+::
+
+    $ gcc mountat_dfd.c
+    $ mktemp -d
+    /tmp/tmp.jz4HILGKEA
+    $ prog=$PWD/a.out
+    $ cd /tmp/tmp.jz4HILGKEA
+
+    $ mkdir -p mnt/{sys,proc}
+    $ tree
+    .
+    └── mnt
+        ├── proc
+        └── sys
+
+    3 directories, 0 files
+    $ sudo "$prog" mnt
+    $ tree -L 3 | tail -1
+    740 directories, 48 files
---
+++ c-prototypes/mountat_dfd.c
@@ -0,0 +1,75 @@
+#include <sys/mount.h>
+
+#include <fcntl.h>
+#include <linux/mount.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/* NB: `mount.h` must be included early:
+ * https://www.mail-archive.com/debian-glibc@lists.debian.org/msg57333.html
+ */
+
+// {{{ Wrapper functions.
+
+/* Function wrappers for the syscalls,
+ * These are not implemented in the c library I use,
+ * These functions are added in `glibc` 2.36:
+ * https://www.phoronix.com/news/GNU-C-Library-Glibc-2.36
+ */
+
+int
+fsopen(const char *fs_name, unsigned int flags)
+{
+    return syscall(SYS_fsopen, fs_name, flags);
+}
+
+int
+fsconfig(int fd, unsigned int cmd, const char *key, const char *value, int aux)
+{
+    return syscall(SYS_fsconfig, fd, cmd, key, value, aux);
+}
+
+int
+fsmount(int fs_fd, unsigned int flags, unsigned int attr_flags)
+{
+    return syscall(SYS_fsmount, fs_fd, flags, attr_flags);
+}
+
+int
+move_mount(int from_dfd, const char *from_pathname, int to_dfd, const char *to_pathname, unsigned int flags)
+{
+    return syscall(SYS_move_mount, from_dfd, from_pathname, to_dfd, to_pathname, flags);
+}
+
+// }}} Wrapper functions.
+
+void
+mountat(int dfd, const char *fstype, const char *source, const char *name)
+{
+    int fd = fsopen(fstype, FSOPEN_CLOEXEC);
+    fsconfig(fd, FSCONFIG_SET_STRING, "source", source, 0);
+    fsconfig(fd, FSCONFIG_CMD_CREATE, NULL, NULL, 0);
+    int mfd = fsmount(fd, FSMOUNT_CLOEXEC, MS_NOEXEC);
+    move_mount(mfd, "", dfd, name, MOVE_MOUNT_F_EMPTY_PATH);
+}
+
+int
+main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        printf("Usage: %s <directory>\n", argv[0]);
+        exit(1);
+    }
+
+    char* dir = argv[1];
+    int dfd = openat(AT_FDCWD, dir, 0);
+    mountat(dfd, "proc", "/proc", "proc");
+    mountat(dfd, "sysfs", "/sys", "sys");
+}
+
+// vim: foldmethod=marker
