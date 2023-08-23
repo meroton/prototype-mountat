9d84adadf0aac937530af9f07006aedfa89354b2
Nils Wireklint <nils@meroton.com>
2023-08-23 16:39:27 +0200

    Successful clone and move
    But not a move from the original location, we just copy the mount.

--- README.rst
+++ README.rst
@@ -27,6 +27,9 @@ Before writing good error-checking go code I wrote these to prototype.
 To understand errors I recommend using `strace`
 to see how the syscalls are called and what they return.
 
+Mountat
+-------
+
 The `mountat_dfd.c` program shows how to create create and place mounts
 into a directory file descriptor,
 which can be created from any path, relative or absolute.
@@ -34,9 +37,9 @@ which can be created from any path, relative or absolute.
 ::
 
     $ gcc mountat_dfd.c
+    $ prog=$PWD/a.out
     $ mktemp -d
     /tmp/tmp.jz4HILGKEA
-    $ prog=$PWD/a.out
     $ cd /tmp/tmp.jz4HILGKEA
 
     $ mkdir -p mnt/{sys,proc}
@@ -50,3 +53,86 @@ which can be created from any path, relative or absolute.
     $ sudo "$prog" mnt
     $ tree -L 3 | tail -1
     740 directories, 48 files
+
+Move mount
+----------
+
+The next exploratory step in trying to unmount the mounts we created.
+This attempts an "Indiana-Jones swap" by moving the mount to a better place,
+that we can address later.
+It should also be a step towards a full unmount,
+which can _allegedly_ be unmounted with `move_mount`, `fspick` and so on.
+
+This [tracee document] is also light but indicates that it should work
+based on the directory file descriptors and names therein.
+But that does not work for me.
+
+::
+
+    $ gcc move_mount.c
+    $ prog=$PWD/a.out
+    $ mktemp -d
+    /tmp/tmp.fcGMUvdIMq
+    $ cd /tmp/tmp.fcGMUvdIMq
+
+    $ mkdir -p {mnt,destination}/proc
+    $ tree
+    .
+    ├── destination
+    │   └── proc
+    └── mnt
+        └── proc
+
+    # Create an initial mount,
+    # as it can be interesting to run the script multiple times,
+    # and it would happily stack mounts,
+    # so it is harder to see when a move or unmount succeeded.
+    $ mount -t proc /proc mnt/proc
+
+    mount -v | grep $PWD
+    /proc on /tmp/tmp.fcGMUvdIMq/mnt/proc type proc (rw,relatime)
+    $ sudo strace -s1000 --failed-only "$prog"
+    mount -v | grep $PWD
+    /proc on /tmp/tmp.fcGMUvdIMq/mnt/proc type proc (rw,relatime)
+    /proc on /tmp/tmp.fcGMUvdIMq/destination/proc type proc (rw,relatime)
+
+This is where I fall short, we are closing in on the solution
+but a full clone is not sufficient,
+we want the original to be unmounted.
+
+The [source file] contains commented out sections that I tried
+combined with their failures.
+Mostly `EINVAL` errors.
+
+They can probably be investigated further by reading warnings and errors
+from the file descriptors,
+or by digging into the Linux source code
+and potentially debugging them.
+But that is a bigger undertaking.
+
+[tracee document]: https://aquasecurity.github.io/tracee/dev/docs/events/builtin/syscalls/move_mount/
+
+Tips and tricks
+---------------
+
+List mounts under the current directory:
+
+    $ mount -v | grep $PWD
+
+Unmount everything below the current directory:
+
+    $ mount -v | cut -d' ' -f3 | xargs -n1 sudo umount
+    $ mount -v | choose 2      | xargs -n1 sudo umount
+
+This unmounts once, so if you have stacked mounts it must be called repeatedly.
+Shout-out to [choose] for many simple `cut` and `awk` use-cases.
+
+looks relative, but it construct an absolute path internally
+
+If we instead create the mount with `mountat` internally
+the mounts will have the `noexec` flag:
+But we still end up with the original and the moved clone.
+
+    /proc on /tmp/tmp.jz4HILGKEA/destination/proc type proc (rw,noexec,relatime)
+
+[choose]: https://github.com/theryangeary/choose
---
+++ c-prototypes/move_mount.c
@@ -0,0 +1,140 @@
+#include <sys/mount.h>
+
+#include <time.h>
+#include <fcntl.h>
+#include <linux/mount.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/* NB: `mount.h` must be included early:
+ * https://www.mail-archive.com/debian-glibc@lists.debian.org/msg57333.html
+ */
+
+/* move_mount examples from the man pages,
+ * to somehow unmount with the Indiana-Jones technique.
+ */
+
+// {{{ Wrapper functions.
+
+int
+fsopen(const char *fs_name, unsigned int flags)
+/* fs/fsopen.c */
+/* SYSCALL_DEFINE2(fsopen, const char __user *, _fs_name, unsigned int, flags) */
+{
+    return syscall(SYS_fsopen, fs_name, flags);
+}
+
+int
+fspick(int dfd, char* path, unsigned int flags)
+/* fs/fsopen.c */
+/* SYSCALL_DEFINE3(fspick, int, dfd, const char __user *, path, unsigned int, flags) */
+{
+    return syscall(SYS_fspick, dfd, path, flags);
+}
+
+int
+fsconfig(int fd, unsigned int cmd, const char *key, const char *value, int aux)
+/* fs/fsopen.c */
+/* SYSCALL_DEFINE5(fsconfig,
+ *   	int, fd,
+ *   	unsigned int, cmd,
+ *   	const char __user *, _key,
+ *   	const void __user *, _value,
+ *   	int, aux)
+ */
+{
+    return syscall(SYS_fsconfig, fd, cmd, key, value, aux);
+}
+
+int
+fsmount(int fs_fd, unsigned int flags, unsigned int attr_flags)
+/* fs/namespace.c */
+/* SYSCALL_DEFINE3(fsmount, int, fs_fd, unsigned int, flags, unsigned int, attr_flags) */
+{
+    return syscall(SYS_fsmount, fs_fd, flags, attr_flags);
+}
+
+int
+move_mount(int from_dfd, const char *from_pathname, int to_dfd, const char *to_pathname, unsigned int flags)
+/* fs/namespace.c */
+/* SYSCALL_DEFINE5(move_mount,
+ *  	int, from_dfd, const char __user *, from_pathname,
+ *  	int, to_dfd, const char __user *, to_pathname,
+ *  	unsigned int, flags)
+ */
+{
+    return syscall(SYS_move_mount, from_dfd, from_pathname, to_dfd, to_pathname, flags);
+}
+
+int
+open_tree(int dfd, const char* filename, unsigned int flags)
+/* fs/namespace.c */
+/* SYSCALL_DEFINE3(open_tree, int, dfd, const char __user *, filename, unsigned, flags) */
+{
+    return syscall(SYS_open_tree, dfd, filename, flags);
+}
+// }}} Wrapper functions.
+
+int
+mountat(int dfd, const char *fstype, const char *source, const char *name)
+{
+    int fd = fsopen(fstype, FSOPEN_CLOEXEC);
+    fsconfig(fd, FSCONFIG_SET_STRING, "source", source, 0);
+    fsconfig(fd, FSCONFIG_CMD_CREATE, NULL, NULL, 0);
+    int mfd = fsmount(fd, FSMOUNT_CLOEXEC, MS_NOEXEC);
+    move_mount(mfd, "", dfd, name, MOVE_MOUNT_F_EMPTY_PATH);
+
+    return mfd;
+}
+
+int
+main(int argc, char* argv[])
+{
+    char* mountpoint = "proc";
+    if (argc < 3) {
+        printf("Usage: %s <initial directory> <destination directory>\n", argv[0]);
+        printf("Moves a mount from one directory to another, using directory file descriptors.\n");
+        exit(1);
+    }
+
+    char* initial = argv[1];
+    char* destination = argv[2];
+    int s_dfd = openat(AT_FDCWD, initial, 0);
+    int d_dfd = openat(AT_FDCWD, destination, 0);
+
+    // Try to use 'move_mount' directly on the mount point.
+    //     code:   move_mount(s_dfd, mountpoint, d_dfd, mountpoint, 0);
+    //     strace: move_mount(3, "proc", 6, "proc", 0)     = -1 EINVAL (Invalid argument)
+
+    // Try to use 'move_mount' on the mount file descriptor from `fspick`.
+    // Pick up, a so called, "filesystem configuration context".
+    //     code:   int cfd = fspick(s_dfd, mountpoint, FSPICK_NO_AUTOMOUNT | FSPICK_CLOEXEC);
+    //     code:   move_mount(cfd, "", d_dfd, mountpoint, 0);
+    //     strace: move_mount(7, "", 6, "proc", 0)         = -1 ENOENT (No such file or directory)
+    //     code:   move_mount(cfd, "", d_dfd, mountpoint, MOVE_MOUNT_F_EMPTY_PATH);
+    //     strace: move_mount(7, "", 6, "proc", MOVE_MOUNT_F_EMPTY_PATH) = -1 EINVAL (Invalid argument)
+    // Must we create a mount again?
+    // So `fspick` is equivalent to `fsopen`.
+    // Additionally, the manpage says that we must reconfigure it
+    //     code:   fsconfig(cfd, FSCONFIG_CMD_RECONFIGURE, NULL, NULL, 0);
+    //     code:   int mfd = fsmount(cfd, FSMOUNT_CLOEXEC, MS_NOEXEC);
+    //     strace: fsmount(5, FSMOUNT_CLOEXEC, MOUNT_ATTR_NOEXEC) = -1 EBUSY (Device or resource busy)
+    // But `fsmount` fails, so this does not seem to be it.
+
+    // Try to use 'move_mount' on the tree file descriptor from 'open_tree'
+    //    code:   int mmfd = open_tree(s_dfd, mountpoint, 0);
+    //    code:   move_mount(mmfd, "", d_dfd, mountpoint, MOVE_MOUNT_F_EMPTY_PATH);
+    //    strace: move_mount(8, "", 6, "proc", MOVE_MOUNT_F_EMPTY_PATH) = -1 EINVAL (Invalid argument)
+
+    // We can successfully move a clone, but not the original mount it seems.
+    int mmmfd = open_tree(s_dfd, mountpoint, OPEN_TREE_CLONE);
+    move_mount(mmmfd, "", d_dfd, mountpoint, MOVE_MOUNT_F_EMPTY_PATH);
+}
+
+// vim: foldmethod=marker
